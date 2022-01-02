40c10a540f455430d9aa7d0b91bc0d54315062f7
jaieve-ps <yoori0303@naver.com>
2022-01-02 16:37:05 +0900

    2.1 install SocketIO

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {WebSocket} from "ws";
+// import {WebSocket} from "ws";
+import SocketIO from "socket.io";
 import http from "http";
 
 const app = express();
@@ -10,37 +11,38 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const httpServer = http.createServer(app);
+const server = http.createServer(app);
+const io = SocketIO(server);
+
+io.on("connection", socket => {console.log(socket)});
+// const wss = new WebSocket.Server({server});
+// const sockets = [];
+//
+//
+// // 여기에서의 socket은 연결된 브라우저를 뜻함
+// wss.on("connection", (socket) => {
+//     // connection이 생기면 socket을 받게 된다.
+//     sockets.push(socket);
+//     socket['nickname'] = "Anon";
+//     socket.on('open', () => {console.log("Connected to Server ❌");})
+//     socket.on("close", () => {
+//         console.log("Disconnected from Server ❌");
+//     })
+//     socket.on("message", (msg) => {
+//         const messageObject = JSON.parse(msg);
+//         switch (messageObject.type){
+//             case "new_message":
+//                 sockets.forEach((aSocket) =>
+//                     aSocket.send(`${socket.nickname}: ${messageObject.payload}`)
+//                 );
+//                 break
+//             case "nickname":
+//                 socket.nickname = messageObject.payload;
+//                 break
+//         }
+//     })
+//     // socket.send() : backend에서 front-end로 보내는 것.
+// })
 
 const handleListen = () => console.log(`Listening on ws://localhost:3000`);
-const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
-const sockets = [];
-
-
-// 여기에서의 socket은 연결된 브라우저를 뜻함
-wss.on("connection", (socket) => {
-    // connection이 생기면 socket을 받게 된다.
-    sockets.push(socket);
-    socket['nickname'] = "Anon";
-    socket.on('open', () => {console.log("Connected to Server ❌");})
-    socket.on("close", () => {
-        console.log("Disconnected from Server ❌");
-    })
-    socket.on("message", (msg) => {
-        const messageObject = JSON.parse(msg);
-        switch (messageObject.type){
-            case "new_message":
-                sockets.forEach((aSocket) =>
-                    aSocket.send(`${socket.nickname}: ${messageObject.payload}`)
-                );
-                break
-            case "nickname":
-                socket.nickname = messageObject.payload;
-                break
-        }
-    })
-    // socket.send() : backend에서 front-end로 보내는 것.
-})
-
 server.listen(3000, handleListen);
