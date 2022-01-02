d12b1e2281ac7353ae96a09d79fc3b13bddbeba9
jaieve-ps <yoori0303@naver.com>
2022-01-02 16:46:17 +0900

    2.2 SocketIO is Amazing!

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,16 @@
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+    event.preventDefault();
+    const input = form.querySelector('input');
+    socket.emit("testroom", {payload : input.value}, () => {
+        console.log("server is done!"); // front 단에서 실행되는 console.log이기 때문에 브라우저의 개발자도구 console에서 print됨.
+    }); //room이라는 이벤트를 보내는 것. 3rd args callback funciton
+    // backend로 object를 보낼 수 있다.
+    input.value="";
+}
+
+form.addEventListener('submit', handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -14,35 +14,14 @@ app.get("/*", (_, res) => res.redirect("/"));
 const server = http.createServer(app);
 const io = SocketIO(server);
 
-io.on("connection", socket => {console.log(socket)});
-// const wss = new WebSocket.Server({server});
-// const sockets = [];
-//
-//
-// // 여기에서의 socket은 연결된 브라우저를 뜻함
-// wss.on("connection", (socket) => {
-//     // connection이 생기면 socket을 받게 된다.
-//     sockets.push(socket);
-//     socket['nickname'] = "Anon";
-//     socket.on('open', () => {console.log("Connected to Server ❌");})
-//     socket.on("close", () => {
-//         console.log("Disconnected from Server ❌");
-//     })
-//     socket.on("message", (msg) => {
-//         const messageObject = JSON.parse(msg);
-//         switch (messageObject.type){
-//             case "new_message":
-//                 sockets.forEach((aSocket) =>
-//                     aSocket.send(`${socket.nickname}: ${messageObject.payload}`)
-//                 );
-//                 break
-//             case "nickname":
-//                 socket.nickname = messageObject.payload;
-//                 break
-//         }
-//     })
-//     // socket.send() : backend에서 front-end로 보내는 것.
-// })
+io.on("connection", socket => {
+    socket.on("testroom", (msg, done) => {
+        console.log(msg); // msg is javascript object!
+        setTimeout(() => {
+            done();
+        }, 1000)
+    })
+});
 
 const handleListen = () => console.log(`Listening on ws://localhost:3000`);
 server.listen(3000, handleListen);
